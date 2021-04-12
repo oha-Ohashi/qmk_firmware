fed55d7effa7f88eb4a293170460570aba7b7f36
Ohashi_uhbie <tmatomatox1@gmail.com>
2021-04-12 09:01:03 +0900

    0412 morning

--- keyboards/ohashi/08_yelo/keymaps/default/keymap.c
+++ keyboards/ohashi/08_yelo/keymaps/default/keymap.c
@@ -24,12 +24,11 @@ enum layer_names {
     _qwe_N,
     _dvorak,
     _N,         //Number Layer
-    _F,         //Functions and Brackets Layer
     _M,         //Marks Layer
     _C,         //Corsur Layer
-    _MS,       //Mouse Controlling Layer
-    _O,       //Others
-    _NULL       //Empty Leyer for creating new one
+    _WF,         //Wheel and Functions
+    _MD,          //controlling Media and 9keys like INS
+    _NULL
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -38,7 +37,7 @@ enum custom_keycodes {
     QMKURL
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_dvorak] = LAYOUT(
+    /*[_dvorak] = LAYOUT(
         KC_TAB,  KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,             KC_ESC,       KC_LGUI, KC_F, KC_G, KC_C, KC_R, KC_L,  LT(_O, KC_BSPC), 
         KC_LSFT, KC_A,    KC_O,    KC_E,   KC_U, KC_I,             KC_LALT,      KC_1,    KC_D, KC_H, KC_T, KC_N, KC_S,  KC_ENT, 
         KC_LCTL, KC_SCLN, KC_Q,    KC_J,   KC_K, LT(_qwe_N, KC_X), KC_SPC,       KC_3,    KC_B, KC_M, KC_W, KC_V, KC_Z,  KC_0
@@ -52,46 +51,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_ESC,       KC_LGUI, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    LT(_O, KC_BSPC), 
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,             KC_LALT,      KC_1,    KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_ENT, 
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, LT(_qwe_N, KC_B), KC_SPC,       KC_3,    KC_N, KC_M, KC_COMM, KC_DOT,KC_SLSH,  KC_0
-    ),
+    ),*/
     [_eucalyn] = LAYOUT(
-        LT(_C, KC_TAB),  LT(_F,KC_DOT), KC_W,         LSFT_T(KC_B), KC_F,        KC_COMM,       ___, ___, LT(_C, KC_M),  KC_R,        LT(_C, KC_D), KC_Y,  KC_P,   LT(_O, KC_BSPC),
-        LCTL_T(KC_SCLN), KC_A,          KC_O,         KC_E,         KC_U,        KC_I,          ___, ___, LT(_F,KC_G),   KC_T,        KC_K,         KC_N,  KC_S,   LCTL_T(KC_MINS),
-        LSFT_T(KC_ESC),  LCTL_T(KC_Z),  LGUI_T(KC_X), LALT_T(KC_C), LT(_C,KC_V), LSFT_T(KC_SPC),___, ___, LT(_M,KC_ENT), LT(_N,KC_H), KC_J,         KC_L,  KC_Q,   LT(_C, _______)
+        LT(_C, KC_DOT),KC_W,         KC_B,         KC_F,        KC_COMM,             LT(_C, KC_M),  KC_R,   LT(_C, KC_D), KC_Y,  KC_P,
+        KC_A,          KC_O,         KC_E,         KC_U,        KC_I,                KC_G,          KC_T,   KC_K,         KC_N,  LT(_WF,KC_S),
+        LCTL_T(KC_Z),  LSFT_T(KC_X), LALT_T(KC_C), LT(_WF,KC_V),LCTL_T(KC_TAB),      LT(_M,KC_BSPC),KC_H,   KC_J,         KC_L,  KC_Q,
+        LT(_C,KC_ESC),                            LT(_M, KC_F15), LSFT_T(KC_SPC),      LT(_N,KC_ENT), LT(_M,KC_F16),                 LT(_MD, KC_LGUI) 
     ),
     [_N] = LAYOUT(
-        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    ___,      ___, _______, KC_PLUS, KC_MINS, KC_ASTR, KC_SLSH, _______, 
-        KC_0,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ___,      ___, _______, KC_LSFT, _______, _______, _______, KC_EQL, 
-        _______,  _______, _______, _______, _______, _______, ___,      ___, _______, _______, _______, _______, _______, _______
+        KC_SLSH, _______, KC_DQUO, KC_QUOT, KC_GRAVE,         KC_COMM, KC_7, KC_8, KC_9, KC_PLUS,
+        KC_COLN, KC_EQL,  KC_UNDS, KC_MINS, KC_TILD,          KC_DOT,  KC_4, KC_5, KC_6, KC_ASTR,
+        KC_SCLN, _______, _______,_______,  _______,          KC_0,    KC_1, KC_2, KC_3, KC_SLSH,
+        KC_BSLS,                  LALT(KC_GRAVE), _______,    _______, KC_0,             _______
     ),
     //Marks
     [_M] = LAYOUT(
-        KC_BSLS, KC_SLSH, KC_QUES, KC_DQUO, KC_QUOT, _______, ___,       ___, ___,    ___, ___, ___, ___, KC_DEL,
-        KC_AMPR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_UNDS, ___,       ___, KC_F16, ___, ___, ___, ___, ___,
-        KC_PIPE, KC_LCBR, KC_RCBR, KC_LABK, KC_RABK, KC_F15,  ___,       ___, ___,    ___, ___, ___, ___, ___
+        KC_SLSH, KC_QUES, KC_AMPR, KC_PIPE, _______,         KC_COMM, KC_7, KC_8, KC_9, KC_PLUS,
+        KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, _______,         KC_DOT,  KC_4, KC_5, KC_6, KC_ASTR,
+        KC_LCBR, KC_RCBR, KC_LABK, KC_RABK,  _______,        KC_0,    KC_1, KC_2, KC_3, KC_SLSH,
+        _______,                  LALT(KC_GRAVE), _______,   _______, KC_0,             _______
     ),
-    //Functions and Brackets
-    [_F] = LAYOUT(
-        ___, _______, KC_PSCR, KC_SLCK, KC_PAUSE, ___, ___,         ___, ___, KC_F7, KC_F8, KC_F9, KC_F12, ___,
-        ___, KC_CAPS, KC_INS,  KC_HOME, KC_PGUP,  ___, ___,         ___, ___, KC_F4, KC_F5, KC_F6, KC_F11, ___,
-        ___, _______, KC_DEL,  KC_END,  KC_PGDN,  ___, ___,         ___, ___, KC_F1, KC_F2, KC_F3, KC_F10, ___ 
-    ),
-    //cursor
+    //Cursor
     [_C] = LAYOUT(
-        _______, KC_WH_R, KC_BSPC, KC_UP,   KC_DEL,   KC_ENT,  ___,       ___, ___, ___, ___, _______, _______, KC_VOLU, 
-        KC_WH_U, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,  ___,       ___, ___, ___, ___, KC_MPRV, KC_MNXT, KC_VOLD,
-        KC_WH_D, KC_WH_L, KC_WH_R, _______, _______,  _______, ___,       ___, ___, ___, ___, KC_MPLY, KC_MUTE, _______
+        ___,     KC_BSPC, KC_UP,   KC_DEL,   KC_ENT,       KC_PGUP, ___, ___, ___, ___,
+        KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,       KC_PGDN, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___,
+        ___,           ___, ___,       ___, ___,           ___
     ),
-    //O
-    [_O] = LAYOUT(
-        TO(_eucalyn),  TO(_qwerty), TO(_dvorak),  ___, ___, ___, ___,        ___, ___, KC_INT4, KC_INT5, KC_BRID, KC_BRIU, _______,     
-        _______,       _______,     _______,      ___, ___, ___, ___,        ___, ___, _______, KC_WHOM, KC_WBAK, KC_WFWD, _______, 
-        _______,       _______,     _______,      ___, ___, ___, ___,        ___, ___, _______, _______, _______, _______, _______
+    //Wheel and Func
+    [_WF] = LAYOUT(
+        ___, ___,     ___,     KC_WH_U, ___,       KC_F12, KC_F7,KC_F8,KC_F9, ___,
+        ___, ___,     ___,     KC_WH_D, ___,       KC_F11, KC_F4,KC_F5,KC_F6, ___,
+        ___, KC_WH_L, KC_WH_R, ___,     ___,       KC_F10, KC_F1,KC_F2,KC_F3, ___,
+        ___,                   ___,     ___,       _______,KC_F10,             ___
+    ),
+    //Media
+    [_MD] = LAYOUT(
+        ___, KC_PSCR,KC_SLCK, KC_PAUS, ___,       ___, ___, ___,     TO(_qwerty), TO(_eucalyn),
+        ___, KC_INS, KC_HOME, KC_PGUP, ___,       ___, ___, KC_MPRV, KC_MNXT,     KC_VOLU,
+        ___, KC_DEL, KC_END,  KC_PGDN, ___,       ___, ___, KC_MPLY, KC_MUTE,     KC_VOLD,
+        ___,           ___, ___,       ___, ___,           ___
     ),
     //Null
     [_NULL] = LAYOUT(
-        ___, ___, ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___, ___, ___,
-        ___, ___, ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___, ___, ___
+        ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___,
+        ___, ___, ___, ___, ___,       ___, ___, ___, ___, ___,
+        ___,           ___, ___,       ___, ___,           ___
     )
 };
 
